31ba8b3a469893c8bd80fc7bd4c446d16b616e98
Doug Wright <mrdougwright@gmail.com>
2020-05-02 11:49:28 -0700

    learning components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,26 @@
 <script>
-	export let name;
+  import Face from "./Face.svelte"
+
+  let say = "hi"
+	setTimeout(() => {
+		say = 'bye'
+	}, 1000)
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  /* affects styling for html in this component */
+  div { color: red }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	/* affects styling globally, for all components in this file? */
+	:global(div) {
+		background: #cbdbf4;
 	}
 </style>
+
+<!-- this component's div -->
+<div> Say: {say} </div>
+
+<!-- this component and others affected by :global -->
+<Face size="4"/>
+<Face size="10"/>
+<Face />
---
+++ src/Face.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let size = 2;
+</script>
+
+<div style="font-size: {size}em">
+  =)
+</div>
+
+
+<!-- Component == Function for HTML, with some JS magic -->
