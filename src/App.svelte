979e685ff8dc36bb9e45e85ad74768af0d474a07
Doug Wright <mrdougwright@gmail.com>
2020-05-02 12:18:59 -0700

    container

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import Face from "./Face.svelte"
+  import Container from "./Container.svelte"
 
   let say = "hi"
 	setTimeout(() => {
@@ -7,20 +8,31 @@
 	}, 1000)
 </script>
 
+
+<Container>
+	<!-- this component's div -->
+	<div> Say: {say} </div>
+
+	<!-- this component and others affected by :global -->
+	<Face index={0} size="10"/>
+	<Face index={1} size="4"/>
+	<Face index={2}/>
+</Container>
+
+
+
+
+
+
 <style>
   /* affects styling for html in this component */
-  div { color: red }
+  div { color: red; }
 
 	/* affects styling globally, for all components in this file? */
-	:global(div) {
-		background: #cbdbf4;
+  :global(*) { box-sizing: border-box; }
+	:global(body, html) {
+		margin: 0;
+		height: 100vh;
+		overflow: hidden;
 	}
 </style>
-
-<!-- this component's div -->
-<div> Say: {say} </div>
-
-<!-- this component and others affected by :global -->
-<Face index={0} size="10"/>
-<Face index={1} size="4"/>
-<Face index={2}/>
